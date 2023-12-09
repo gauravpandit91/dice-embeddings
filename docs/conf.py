c743d40fceb54a0c0088938e73ee62974f23f3cb
arymandeshwal <deshwal.aryman@gmail.com>
2023-12-09 21:57:51 +0100

    Reverting to main branch

--- docs/conf.py
+++ docs/conf.py
@@ -8,30 +8,24 @@
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
+sys.path.insert(0, os.path.abspath(".."))
 
-project = 'Sphinx-docs'
-copyright = '2023, Aryman'
-author = 'Aryman'
-release = '0.1'
+project = 'DICE Embeddings'
+copyright = '2023, Caglar Demir'
+author = 'Caglar Demir'
+release = '0.1.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
 templates_path = ['_templates']
-exclude_patterns = []
-
-
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 html_static_path = ['_static']
